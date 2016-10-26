2172c194bcd72c19c36e1c9069219756d103fb3e
Graeme Crawley <gacrawley@gmail.com>
2016-10-26 02:23:52 -0400

    Created JSON to store clothing sizes. Averaged ranges and designed table to be pullable for later

--- american_apparel.py
+++ american_apparel.py
@@ -2,6 +2,7 @@ from lxml import html
 from bs4 import BeautifulSoup
 import requests
 import urllib2
+import json
 
 site = "https://www.americanapparel.net/sizing/default.asp?chart=womens.shirts"
 
@@ -28,18 +29,54 @@ table1 = soup('td', {'style': 'padding-top:7px'})
 
 table2 = table1[0].find_all('td')
 
-
+data =[]
+prefix = ""
+p = 0
+trying = [[]]
+i = 0
+j = 0
+p = 0
 for row in table1[0].find_all('tr'):
-        for col in row.find_all('td'):
-            try:
-                if col.b.string != None:
-                    print col.b.string
-                    continue
-            except AttributeError, e:
-                pass
-            try:
-                if col.div.string != None:
-                    print col.div.string
-            except AttributeError, e:
-                pass
+    if p == 0:
+        p = 1
+        continue
+    item = {"title": row.find_all('td')[0].b.string}
+    for col in row.find_all('td'):
+        try:
+            if col.b.string != None:
+                #item[table1[0].find_all('tr')[0].find_all('td')[i].b.string] = col.b.string
+                #print col.b.string
+                #print j, " ", i
+                i = i + 1
+                continue
+        except AttributeError, e:
+            pass
+        try:
+            if col.div.string != None:
+                #print "hello"
+                #print table1[0].find_all('tr')[0].find_all('td')[i].b.string
+
+                if("-" in col.div.string):
+                    values = col.div.string.split('-')
+                    average = int(values[0])+1
+                    print average
+                else:
+                    average = col.div.string
+                item[average] = table1[0].find_all('tr')[0].find_all('td')[i].b.string
+                #print "hello"
+                #item[col.div.string] = col.div.string
+                #print col.div.string
+                #print j, " ", i
+
+        except AttributeError, e:
+            pass
+        i = i + 1
+    print item
+    data.append(item)
+    i = 0
+    j = j + 1
+
+jsonData = json.dumps(data)
+
+print jsonData
 
